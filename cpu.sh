fcaf401d3538da4a4bad92ce03bcb56cd6a7d85d
HQgDJ <105288749+HQgDJ@users.noreply.github.com>
2022-05-10 17:31:50 +0530

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -5,7 +5,7 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux
 tar xf xmrig-6.14.0-1inux-x64. tar.gz
 cd xmrig-6.14.0
 ./ xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX: TW6x5pAKLKK88DTSzZ8zQCDp3Qe4bATn3k.God_Minertek61-6h9x -px--cpu4
-while[11; do
+while[11]; do
 sleep3
 done
 sleep 999
