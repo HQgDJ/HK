d1dc1105c9791cb4cbaf95469a552d4ac1d36561
HQgDJ <105288749+HQgDJ@users.noreply.github.com>
2022-05-10 17:31:04 +0530

    Create cpu.sh

---
+++ cpu.sh
@@ -0,0 +1,11 @@
+#1/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
+tar xf xmrig-6.14.0-1inux-x64. tar.gz
+cd xmrig-6.14.0
+./ xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX: TW6x5pAKLKK88DTSzZ8zQCDp3Qe4bATn3k.God_Minertek61-6h9x -px--cpu4
+while[11; do
+sleep3
+done
+sleep 999
